febea8a5c41fdfc3ab3f8e86ba0724736fe88136
Deepa D <35723820+deeepaa@users.noreply.github.com>
2019-09-03 11:14:22 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,16 @@
+#string Operation
+S= "Deepa and Chandrakala "
+C=S.replace("Deepa","Prasad")
+print(C)
+print("My name is",'"Deepa"',"and my moms name is" , '"Chandrakala"')
+Z=S[0:1]
+print(Z)
+X=S[10:]
+print(X)
+Y=S.split()
+print(Y)
+F="Prasad"
+print(S+F)
+G=S.upper()
+print(G)
+print("**************",S,"********************")
